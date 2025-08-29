21123d0e6bf86ccda68fa6bbbfe3330134b355b7
Troy Smith <troy.smith@uga.edu>
2025-08-29 02:11:24 -0400

    UI

--- notebooks/utils/io.py
+++ notebooks/utils/io.py
@@ -7,6 +7,30 @@ import shutil
 from .writing import orca_input_and_sh
 
 
+def generate_guess(smiles: str, job_type: str, label: str):
+    identifier = (
+        smiles.replace("[", "_")
+        .replace("]", "_")
+        .replace("(", "ch_")
+        .replace(")", "_hc")
+        .replace("=", "dbl")
+        .replace("#", "trpl")
+        .replace("/", "up")
+        .replace("\\", "dwn")
+    )
+    output_dir = (
+        Path.home()
+        / f"C5O-Kinetics/calc/{identifier}/Optimization/run/{job_type}_{label}/init.xyz"
+    )
+    mol_format = Chem.AddHs(Chem.MolFromSmiles(smiles))
+    AllChem.EmbedMolecule(mol_format)
+    charge = Chem.GetFormalCharge(mol_format)
+    multiplicity = Descriptors.NumRadicalElectrons(mol_format) + 1
+    # Write guess xyz
+    xyz = Chem.MolToXYZBlock(mol_format)
+    output_dir.write_text(xyz.lstrip())
+
+
 def get_xyz(smiles: str = "[CH]1CO1", method: str = "REVDSD"):
     identifier = (
         smiles.replace("[", "_")
@@ -18,10 +42,9 @@ def get_xyz(smiles: str = "[CH]1CO1", method: str = "REVDSD"):
         .replace("/", "up")
         .replace("\\", "dwn")
     )
-    dir = Path.home() / f"C5O-Kinetics/calc/{identifier}/Optimization/run/{method}"
-    unique_files = {f.name: f for f in dir.rglob("*xyz")}
-    unique_files_list = [(name, f) for name, f in unique_files.items()]
-    return sorted(unique_files_list, key=lambda name: name[0])
+    xyz_dir = Path.home() / f"C5O-Kinetics/calc/{identifier}/Optimization/run/{method}"
+    unique_files = {f.name: f for f in xyz_dir.rglob("*xyz")}
+    return sorted(unique_files.items(), key=lambda x: x[0])
 
 
 def get_transitions(smiles: str = "[CH]1CO1"):
@@ -35,47 +58,209 @@ def get_transitions(smiles: str = "[CH]1CO1"):
         .replace("/", "up")
         .replace("\\", "dwn")
     )
-    dir = Path.home() / f"C5O-Kinetics/calc/{identifier}"
-    files = [(f.name, f) for f in dir.iterdir() if f.is_dir()]
-    return sorted(files, key=lambda name: name[0])
+    trans_dir = Path.home() / f"C5O-Kinetics/calc/{identifier}"
+    return sorted(
+        [(f.name, f) for f in trans_dir.iterdir() if f.is_dir()], key=lambda x: x[0]
+    )
 
 
 def get_scan_xyzs(trans_dir: Path):
-    dir = trans_dir / "run/REVDSD_Scan"
-    files = [(str(f).split("REVDSD_Scan")[1], f) for f in dir.rglob("*xyz")]
-    return sorted(files, key=lambda name: name[0])
+    scan_dir = trans_dir / "run/REVDSD_Scan"
+    files = [(str(f).split("REVDSD_Scan")[1], f) for f in scan_dir.rglob("*xyz")]
+    return sorted(files, key=lambda x: x[0])
 
 
 def get_energies(xyz_path: Path):
-    # --- Update energies ---
     run_dir = Path(str(xyz_path).split("run")[0]) / "run"
-    revdsd_logs = [x for x in (run_dir / "REVDSD").rglob("REVDSD.log")]
+    # ZPV energies
+    revdsd_logs = list((run_dir / "REVDSD").rglob("REVDSD.log"))
     zpv_energies = []
     for log in revdsd_logs:
-        with open(str(log), "r") as f:
-            zpv_energies += (
+        with open(log, "r") as f:
+            zpv_energies += [
                 line.split("Eh")[1].strip() for line in f if "Zero point energy" in line
-            )
+            ]
     zpv_text = (
-        "ZPV: " + ",".join(e for e in zpv_energies)
+        f"ZPV: {','.join(zpv_energies)}"
         if zpv_energies
         else "Zero Point Vibrational Energy: --"
     )
-    ccsdt_logs = [x for x in (run_dir / "CCSDT").rglob("CCSDT.log")]
+    # SPC energies
+    ccsdt_logs = list((run_dir / "CCSDT").rglob("CCSDT.log"))
     spc_energies = []
     for log in ccsdt_logs:
-        with open(str(log), "r") as f:
-            spc_energies += (
+        with open(log, "r") as f:
+            spc_energies += [
                 line.split("ENERGY")[1].strip()
                 for line in f
                 if "FINAL SINGLE POINT ENERGY" in line
-            )
+            ]
     spc_text = (
-        "SPC (hartrees): " + ",".join(e for e in spc_energies)
+        f"SPC (hartrees): {','.join(spc_energies)}"
         if spc_energies
         else "Single Point Calculation Energy: --"
     )
     return zpv_text, spc_text
+
+
+def orca_optimization(
+    method: str,
+    smiles: str,
+    charge: int = 0,
+    multiplicity: int = 2,
+    job_type: str = "opt",
+    guess: bool = False,
+) -> None:
+    """
+    Write an ORCA input file for various calculation types.
+
+    job_type options:
+        - "opt": Optimization only (no frequency calculation) (default)
+        - "opt_freq": Optimization and frequency calculation
+        - "goat": Geometry optimization and frequency calculation (GOAT)
+        - "elec": Single-point electronic structure calculation
+    """
+    identifier = (
+        smiles.replace("[", "_")
+        .replace("]", "_")
+        .replace("(", "ch_")
+        .replace(")", "_hc")
+        .replace("=", "dbl")
+        .replace("#", "trpl")
+        .replace("/", "up")
+        .replace("\\", "dwn")
+    )
+    if "XTB" in method:
+        label = "XTB"
+        job_name = f"{smiles}_xtb"
+        num_cpus = 8
+        mem_per_cpu = 1
+        lscratch_size = 10
+        time = "00:30:00"
+    elif "REVDSD" in method:
+        label = "REVDSD"
+        job_name = f"{smiles}_revdsd"
+        num_cpus = 16
+        mem_per_cpu = 1
+        lscratch_size = 20
+        time = "8:00:00"
+    elif "CCSD(T)" in method:
+        label = "CCSDT"
+        job_name = f"{smiles}_ccsdt"
+        if smiles.count("C") + smiles.count("O") >= 8:
+            num_cpus = 20
+            mem_per_cpu = 12
+            lscratch_size = 200
+            time = "3:00:00"
+        elif smiles.count("C") + smiles.count("O") == 7:
+            num_cpus = 20
+            mem_per_cpu = 6
+            lscratch_size = 200
+            time = "1:00:00"
+        elif smiles.count("C") + smiles.count("O") == 6:
+            num_cpus = 20
+            mem_per_cpu = 4
+            lscratch_size = 200
+            time = "1:00:00"
+        elif smiles.count("C") + smiles.count("O") == 5:
+            num_cpus = 16
+            mem_per_cpu = 4
+            lscratch_size = 200
+            time = "1:00:00"
+        elif smiles.count("C") + smiles.count("O") == 4:
+            num_cpus = 12
+            mem_per_cpu = 4
+            lscratch_size = 100
+            time = "00:30:00"
+        elif smiles.count("C") + smiles.count("O") == 3:
+            num_cpus = 12
+            mem_per_cpu = 2
+            lscratch_size = 50
+            time = "00:15:00"
+        elif smiles.count("C") + smiles.count("O") == 2:
+            num_cpus = 4
+            mem_per_cpu = 1
+            lscratch_size = 50
+            time = "00:15:00"
+        elif smiles.count("C") + smiles.count("O") == 1:
+            num_cpus = 4
+            mem_per_cpu = 1
+            lscratch_size = 50
+            time = "00:15:00"
+    else:
+        raise ValueError(f"Unknown method: {method}")
+
+    # --- ORCA Input File
+    par_line = "# --- ORCA Parameters\n"
+    par_line += f"%PAL NPROCS {num_cpus} END\n"  # Here
+    par_line += f"%maxcore {mem_per_cpu * 750}\n\n"  # Here
+    job_line = "# --- Job Parameters\n"
+    if job_type == "opt":
+        job_line += f"! {method} OPT\n\n"
+    elif job_type == "opt_freq":
+        job_line += f"! {method} OPT NumFreq\n\n"
+    elif job_type == "opt_freq_ts":
+        job_line += f"! {method} OPTTS NumFreq\n\n%geom\n    Calc_Hess true\n    NumHess true\nend\n\n"
+    elif job_type == "goat":
+        job_line += f"! {method} GOAT\n\n"
+    elif job_type == "elec":
+        job_line += f"! {method}\n\n"
+    else:
+        raise ValueError(f"Unknown job_type: {job_type}")
+
+    xyz_line = "# --- XYZ Parameters\n"
+    xyz_line += f"* xyzfile {charge} {multiplicity} init.xyz\n"
+
+    txt = f"{par_line}{job_line}{xyz_line}"
+    output_dir = (
+        Path.home()
+        / f"C5O-Kinetics/calc/{identifier}/Optimization/run/{job_type}_{label}"
+    )
+    output_dir.mkdir(parents=True, exist_ok=True)
+    path_out = output_dir / f"{label}.inp"
+    if not path_out.exists():
+        path_out.write_text(txt)
+    # ----- Submission Shell Script
+    # --- SLURM Parameters
+    slurm_opts = (
+        "#!/bin/bash\n"
+        f"#SBATCH --partition=batch\n"
+        f"#SBATCH --gres=lscratch:{lscratch_size}\n"
+        f"#SBATCH --job-name={job_name}\n"
+        "#SBATCH --nodes=1\n"
+        f"#SBATCH --ntasks={num_cpus}\n"
+        f"#SBATCH --ntasks-per-node={num_cpus}\n"  # Here
+        "#SBATCH --cpus-per-task=1\n"
+        f"#SBATCH --time={time}\n"
+        f"#SBATCH --mem-per-cpu={mem_per_cpu}G\n\n"  # Here
+    )
+    # --- ORCA Module Initialization
+    module_opts = "# --- Load ORCA 6.1\n"
+    module_opts += "module load ORCA/6.1\n"
+    module_opts += "cd ${SLURM_SUBMIT_DIR}\n\n"
+    # --- Lscratch Directory Initialization
+    lscratch_opts = "# --- Initialize lscratch directory\n"
+    lscratch_opts += "mkdir -p /lscratch/${USER}/${SLURM_JOB_ID}\n"
+    lscratch_opts += 'cp -r "${SLURM_SUBMIT_DIR}/." /lscratch/${USER}/${SLURM_JOB_ID}\n'
+    lscratch_opts += "cd /lscratch/${USER}/${SLURM_JOB_ID}\n\n"
+    # --- Append Cleanup and Copy Commands
+    job_opts = (
+        "# --- Run orca\n"
+        f"$(which orca) {label}.inp > {label}.log\n\n"
+        "# --- Extract important files\n"
+        "cp -r /lscratch/${USER}/${SLURM_JOB_ID} ${SLURM_SUBMIT_DIR}\n"
+        "rm -rf /lscratch/${USER}/${SLURM_JOB_ID}\n"
+    )
+    # --- Combine All Parts and Write to File
+    script = slurm_opts + module_opts + lscratch_opts + job_opts
+    path_out = Path(output_dir) / f"submit_{label}.sh"
+    if not path_out.exists():
+        path_out.write_text(script)
+
+    if guess is True:
+        generate_guess(smiles=smiles, job_type=job_type, label=label)
+
+    return output_dir, label, identifier
 
 
 def optimization(
--- notebooks/utils/visualize.py
+++ notebooks/utils/visualize.py
@@ -1,8 +1,13 @@
+import altair as alt
+import pandas as pd
 import py3Dmol
+import pyparsing as pp
+import pyparsing.common as ppc
 from rdkit import Chem
 from rdkit.Chem import AllChem
 
 from pathlib import Path
+import re
 
 
 def visualize_smiles(smiles: str, width: int = 600, height: int = 350):
@@ -67,3 +72,36 @@ def visualize_traj(xyz_path, width: int = 600, height: int = 350):
     viewer.zoomTo()
     viewer.animate({"loop": "backAndForth"})
     viewer.show()
+
+
+def visualize_scan_energy(xyz_path: Path):
+    allxyz_path = xyz_path.parent / "REVDSD.allxyz"
+
+    comment = pp.Group(
+        pp.Keyword("Scan Step")
+        + ppc.integer("index")
+        + pp.Keyword("E")
+        + ppc.fnumber("energy")
+    )
+    expr = pp.OneOrMore(pp.SkipTo(comment, include=True))
+    text = re.sub(">\n", "", allxyz_path.read_text())
+
+    indices = []
+    energies = []
+    results = expr.parse_string(text)
+    for result in results[1::2]:
+        index, energy = result[1:4:2]
+        indices.append(index)
+        energies.append(energy)
+
+    df = pd.DataFrame(
+        {
+            "index": indices,
+            "energy": energies,
+        }
+    )
+
+    alt.Chart(df).mark_point().encode(
+        x="index",
+        y=alt.Y("energy", scale=alt.Scale(zero=False)),
+    ).show()
